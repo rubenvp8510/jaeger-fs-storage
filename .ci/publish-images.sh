69ae4ca85bdb34ed94abcd09132e0175e7d94df8
Ruben Vargas <ruben.vp8510@gmail.com>
2020-10-09 22:40:47 -0500

    Github action to push images
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>

---
+++ .ci/publish-images.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [ "${DOCKER_PASSWORD}x" != "x" -a "${DOCKER_USERNAME}x" != "x" ]; then
+    echo "Performing a 'docker login'"
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+fi
+make docker
+make docker-push
