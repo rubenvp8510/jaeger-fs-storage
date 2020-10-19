85691349f212b9c411f38c72295be3929b2510f2
Ruben Vargas <ruben.vp8510@gmail.com>
2020-10-19 16:14:03 -0500

    Remove unnecesary steps for performance action
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>

--- .ci/download-fixtures.sh
+++ .ci/download-fixtures.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-FIXTURE_PATH=data/traces
+FIXTURE_PATH=./data/traces
 FILE_ID="15ub8SQtHwt3S2W1niHGr2_8lFtDw-JXi"
-if [[ -f FIXTURE_PATH ]]; then
-   echo "File $FILE exists, skipping download."
+if [[ -f ${FIXTURE_PATH} ]]; then
+   echo "File $FIXTURE_PATH exists, skipping download."
 else
     mkdir -p ./data
     curl -L -o ./data/traces https://www.dropbox.com/s/dyuk2b9aeysuznq/traces?dl=1
